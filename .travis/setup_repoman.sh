2d026c3c3fc57df0876646994b1d344b7efc1752
Mark Gomersbach <markgomersbach@gmail.com>
2020-04-19 10:59:06 +0200

    Rename file
    Signed-off-by: Mark Gomersbach <markgomersbach@gmail.com>

--- .travis/setup_repoman.sh
+++ .travis/setup_repoman.sh
@@ -7,9 +7,12 @@ chmod a+rwX /etc/passwd /etc/group /etc /usr /var/db/repos /var /var/cache
 echo "portage:x:250:250:portage:/var/tmp/portage:/bin/false" >> /etc/passwd
 echo "portage::250:portage,travis" >> /etc/group
 
-cp -r  * /var/db/repos/${OVERLAY_NAME}/ || echo "Could not move repo"
+cp -r  * /var/db/repos/${OVERLAY_NAME}/ || echo "Could not copy repo"
 
+# Get portage utils
 wget -qO - "https://github.com/gentoo/portage/archive/portage-${PORTAGE_VER}.tar.gz" | tar xz -C /tmp
+
+# Get portage tree
 wget -qO - "https://github.com/gentoo-mirror/gentoo/archive/master.tar.gz" | tar xz -C /var/db/repos/gentoo --strip-components=1
 
 cp .travis/gentoo.conf /etc/portage/repos.conf/ || echo "Could not copy gentoo repo config"
